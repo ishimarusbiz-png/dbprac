9921841229daca3a26303df14f2b872404aead07
ishimarusbiz-png <ishimaru.s.biz@gmail.com>
2026-03-22 14:29:15 +0900

    ページを修正

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas
 import pymysql
 from flask import Flask, render_template, request, redirect, url_for
 import os
+from db import LDM
 
 # Flaskアプリケーションのインスタンス化
 app = Flask(__name__)
@@ -35,11 +36,30 @@ def main_page():
     # ログイン成功後に表示したいHTMLファイルを指定（例: index.html）
     return render_template("index.html")
 
+db_manager = LDM()
+
 @app.route("/apppage")
 def app_page():
-    # ログイン成功後に表示したいHTMLファイルを指定（例: index.html）
-    return render_template("apppage.html")
-
+    db_manager.connect()
+    # 辞書形式で取得できるように設定（Cursorクラスを変更するか手動変換）
+    db_manager.cursor.execute("SELECT IpId, TimeStamp, Uri, HttpMethod, ResponseCode, Bytes, Referrer, UserAgent FROM ips  ORDER BY IpId LIMIT 15")
+    rows = db_manager.cursor.fetchall()
+    
+    # テンプレートに渡すデータ（辞書のリストにする例）
+    log_data = []
+    for r in rows:
+        log_data.append({
+            "ipid": r[0],
+            "timestamp": r[1],
+            "uri": r[2],
+            "method": r[3],
+            "status": r[4],
+            "bytes": r[5],
+            "referrer": r[6],
+            "ua": r[7]
+        })
+    
+    return render_template("apppage.html", log=log_data)
 
 # サーバーの起動
 if __name__ == "__main__":
